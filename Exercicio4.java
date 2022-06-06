2da6a0a6f6ced91f0f6c46c59b16940ab598729e
joaofaggion17 <joaofaggion17@gmail.com>
2022-06-06 18:08:48 +0000

    Salvando

---
+++ Exercicio4.java
@@ -0,0 +1,11 @@
+public class Exercicio4{
+
+    public static void main(String [] args){
+
+        for (int index = 38; index < 1001; index++) {
+            System.out.println(" Número: " + index);
+
+            
+        }
+    }
+}
